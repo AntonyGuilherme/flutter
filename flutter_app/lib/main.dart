224cc971a497b2686e0d9940cbd4993fb5dccd03
=Antony <guilhermecosta994@gmail.com>
2021-05-08 10:38:14 -0300

    Criando um StateLessWidget

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -18,21 +18,36 @@ class FriendsApp extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  MyFriend('Antony'),
                   Padding(
                       padding: const EdgeInsets.all(10),
                       child: DecoratedBox(
                         decoration: BoxDecoration(color: Colors.lightBlue),
-                        child: Text('Maria', style: TextStyle(fontSize: 50)),
-                      )),
-                  Padding(
-                      padding: const EdgeInsets.all(10),
-                      child : DecoratedBox(
-                        decoration: BoxDecoration(color: Colors.lightBlue),
-                        child: Text('Gabriel', style: TextStyle(fontSize: 50),),
-                      )
-                  )
+                        child: Text(
+                          'Gabriel',
+                          style: TextStyle(fontSize: 50),
+                        ),
+                      ))
                 ],
               ),
             )));
+  }
+}
+
+//StatelessWidget - no dinamic
+class MyFriend extends StatelessWidget {
+  final String text;
+
+  const MyFriend(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: DecoratedBox(
+        decoration: BoxDecoration(color: Colors.lightBlueAccent),
+        child: Text(text, style: TextStyle(fontSize: 50)),
+      ),
+    );
   }
 }
