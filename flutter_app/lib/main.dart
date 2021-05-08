b71abeb9b74f8ec7ec2523bc74d965da14ea9a35
=Antony <guilhermecosta994@gmail.com>
2021-05-08 09:09:54 -0300

    Exemplo utilizando um contador - States

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,49 +1,82 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  /*runApp(Center(
-      child: Text('Hello, world!',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 30.0))));*/
+void main() => runApp(MyApp());
 
-  runApp(MaterialApp(
-    title: 'Flutter Tutorial',
-    home: TutorialHome()
-  ));
+class MyApp extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Livro sobre Fultter',
+      home: MyHomePage(),
+    );
+  }
 }
 
+class MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-class TutorialHome extends StatelessWidget{
+class _MyHomePageState extends State<MyHomePage> {
+
+  /* No Dart tudo que contém underline antes do nome é privado */
+
+  int _counter = 0;
+
+  void _incrementCounter(){
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void alert(String message){
+    print(message);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: 'Menu de Navegação',
-          onPressed: null
+        appBar: AppBar(
+          title: Text('Nosso aplicativo'),
+          backgroundColor: Colors.brown,
         ),
-          title : Text('Title'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: 'Buscar',
-            onPressed: null,
-          )
-        ]
-      ),
-      body: Center(
-          child: Text('Olá Mundo!')
-      ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Adicionar',
-        child: Icon(Icons.add),
-        onPressed: null,
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Vezes que o botão foi apertado'),
+                Text('$_counter',style: Theme.of(context).textTheme.display1,)
+              ]
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          child: Icon(Icons.add),
+          backgroundColor: Colors.red,
+        ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.brown,
+        child: Container(
+          height: 100,
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.add_box, color: Colors.white),
+                    onPressed: () { alert('Adcionei qualquer coisa'); }
+                    ),
+                IconButton(
+                    icon: Icon(Icons.add_a_photo, color : Colors.white),
+                    onPressed: () { alert('Adicionei uma foto'); })
+
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 
-}
 
+}
